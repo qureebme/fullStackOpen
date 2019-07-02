810afb80511492bbd6f41ce7fb447790b9af741c
Qureeb Hameed <quareebs@yahoo.com>
2019-07-02 12:45:08 +0300

    Completed exercise 1.3, Atom introduced for proper formatting

--- part1_exercise/part1_app/src/index.js
+++ part1_exercise/part1_app/src/index.js
@@ -5,85 +5,46 @@ import ReactDOM from 'react-dom';
 //import * as serviceWorker from './serviceWorker';
 
 const Header = (props) => {
-    return ( <
-        h1 > { props.course } < /h1>
-    )
-}
-
-const Part = (props) => {
-    return ( <
-        >
-        <
-        p > { props.part } { props.ex } <
-        /p>   < / >
-    )
-}
-
-const Content = () => {
-
-    const part = {
-        part1: 'Fundamentals of React',
-        part2: 'Using props to pass data',
-        part3: 'State of a component'
-    }
-    const ex = { exercises1: 10, exercises2: 7, exercises3: 14 }
-
-    return ( <
-        div >
-        <
-        Part part = { part.part1 }
-        ex = { ex.exercises1 }
-        / > <
-        Part part = { part.part2 }
-        ex = { ex.exercises2 }
-        / > <
-        Part part = { part.part3 }
-        ex = { ex.exercises3 }
-        / > < /
-        div >
+    return ( <h1> { props.course } </h1>
     )
 }
 
 const Total = (props) => {
-    return ( <
-        p > Number of exercises { props.exercises1 + props.exercises2 + props.exercises3 } < /p>
+    return (
+      <p> Number of exercises { props.part1x + props.part2x + props.part3x } </p>
     )
 }
 
-
 const App = () => {
-        // const-definitions
-        const course = 'Half Stack application development'
 
-        const exercises1 = 10
-        const exercises2 = 7
-        const exercises3 = 14
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-        return ( <
-            >
-            <
-            Header course = { course }
-            /> 
-
-            <
-            Content /
-            >
-            <
-
-            Total exercises1 = { exercises1 }
-            exercises2 = { exercises2 }
-            exercises3 = { exercises3 }
-            / > < / >
-
-        )
-    }
-    /*
-    
-    */
+  return (
+    <div>
+      <Header course = { course }/>
+      <p> {part1.name} {part1.exercises} </p>
+      <p> {part2.name} {part2.exercises} </p>
+      <p> {part3.name} {part3.exercises} </p>
+      <Total part1x={part1.exercises} part2x={part2.exercises} part3x={part3.exercises}/>
+    </div>
+  )
+}
 
 ReactDOM.render( < App / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+//serviceWorker.unregister()
