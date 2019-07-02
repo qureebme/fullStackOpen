718af4e0ca10a6cca8781a19f32c96ab26bb97d1
Qureeb Hameed <quareebs@yahoo.com>
2019-07-02 13:29:36 +0300

    Completed ex 1.4, exact specs

--- part1_exercise/part1_app/src/index.js
+++ part1_exercise/part1_app/src/index.js
@@ -9,20 +9,20 @@ const Header = (props) => {
     )
 }
 
-const Total = (props) => {
-    return (
-      <p> Number of exercises { props.part1x + props.part2x + props.part3x } </p>
-    )
-}
-
 const Content = (props) => {
   return(
     <>
-    <p>{props.name0} {props.ex0}</p>
-    <p>{props.name1} {props.ex1}</p>
-    <p>{props.name2} {props.ex2}</p>
+    <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+    <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+    <p>{props.parts[2].name} {props.parts[2].exercises}</p>
     </>
   )
+}
+
+const Total = (props) => {
+    return (
+      <p> Number of exercises { props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises } </p>
+    )
 }
 
 const App = () => {
@@ -46,9 +46,8 @@ const App = () => {
   return (
     <div>
       <Header course = { course }/>
-      <Content name0={parts[0].name} name1={parts[1].name} name2={parts[2].name}
-      ex0={parts[0].exercises} ex1={parts[1].exercises} ex2={parts[2].exercises}/>
-      <Total part1x={parts[0].exercises} part2x={parts[1].exercises} part3x={parts[2].exercises}/>
+      <Content parts = { parts } />
+      <Total parts={parts} />
     </div>
   )
 }
