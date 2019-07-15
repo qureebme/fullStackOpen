edfb6fe3dcdec8e315a03d95ec9fb7710f86739e
Qureeb Hameed <quareebs@yahoo.com>
2019-07-15 23:22:52 +0300

    added part2.1: course content step6. ...getting back my messed-up commits!

---
+++ part2/src/components/course.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+
+const Course = ({course}) =>(
+  <>
+  <h1>{course.name}</h1>
+  <h2>{"Contents"}</h2>
+  <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+
+  <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+
+  <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+
+  </>
+)
+
+
+export default Course
