9cf56bf41e75cc7e8a1b33f7bf63b6286c02aa9e
Qureeb Hameed <quareebs@yahoo.com>
2019-07-17 23:35:40 +0300

    Final solution. Keys are UNIQUE, yet key error persists. STRANGE!

--- part2/src/components/course.js
+++ part2/src/components/course.js
@@ -3,19 +3,24 @@ import React from 'react';
 
 //returns the items under each course part
 const Course = ({courseParts}) => courseParts.map((each)=>
-  <li key={each.id + Math.random().toString()}>{each.name} {each.exercises}</li>
+  <li key={each.id}>{each.name} {each.exercises}</li>
     )
 
-const Courses = ({courses}) =>courses.map((eachCourse)=>(
+const Courses = ({courses}) => courses.map((eachCourse)=>(
     <div>
-      <h2 key={eachCourse.id + courses.length.toString()}>{eachCourse.name}</h2>
+      <h2 key={eachCourse.id}>{eachCourse.name}</h2>
       <ul>
-        <Course courseParts={eachCourse.parts}/>
+      {eachCourse.parts.map(each => <li key={eachCourse.id.toString()+each.id.toString()}>{each.name} {each.exercises}</li>)}
       </ul>
+
       <p><b>Total of {eachCourse.parts.reduce((sum, each)=> sum+each.exercises, 0)} exercises</b></p>
     </div>)
     )
 
-
+/*
+<ul>
+  <Course courseParts={eachCourse.parts}/>
+</ul>
+*/
 
 export default Courses
