ca65f0618492a47aa46a9250e6cf881d220e10f8
Qureeb Hameed <quareebs@yahoo.com>
2019-07-18 12:10:39 +0300

    added part 2.8: phonebook step3

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -2,32 +2,36 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]);
   const [ newName, setNewName ] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const onSubmitHandler = (e) =>{
     e.preventDefault();
 
-    let newNameList = persons.concat({name: newName});
+    let newNameList = persons.concat({name: newName.trim(), number: newNumber.trim()});
     let checker = newNameList.filter((currentObj) => currentObj.name.toLowerCase()===newName.toLowerCase());
 
     if (checker.length !== 1) alert(`${newName} is already added to phonebook`);
     else{
       setPersons(newNameList);
       setNewName('');
+      setNewNumber('');
     }
   }
 
   const onChangeHandler = (e) => setNewName(e.target.value);
+  const onChangeHandler2 = (e) => setNewNumber(e.target.value);
 
-  const showAllNames = (personsArray) => personsArray.map(each => <li key={each.name}>{each.name}</li>)
+  const showAllNames = (personsArray) => personsArray.map(each => <li key={each.name}>{each.name} {each.number}</li>)
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={onSubmitHandler}>
         <div>
-          name: <input value={newName} onChange={onChangeHandler}/>
+          name: <input value={newName} onChange={onChangeHandler}/><br/><br/>
+          number: <input value={newNumber} onChange={onChangeHandler2}/>
         </div>
         <div>
           <button type="submit">add</button>
