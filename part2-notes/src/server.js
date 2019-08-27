c24a1897021d2c1b46df580a9eee61213f7ccac2
Qureeb Hameed <quareebs@yahoo.com>
2019-08-27 15:45:48 +0300

    server.js modified to work with back end

--- part2-notes/src/server.js
+++ part2-notes/src/server.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const dataURI = 'http://localhost:3001/persons/';
+const dataURI = 'http://localhost:3001/api/persons/';
 
 const loadStartData = () => {
     let req = axios.get(dataURI)
